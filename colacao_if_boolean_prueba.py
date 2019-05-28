ef73be4112e61e6ff4f05ca3d933482f0acac161
Nicolasgeekit <nicolas.salazar.it@gmail.com>
2019-05-28 13:16:47 -0300

    PRUEBA

---
+++ colacao_if_boolean_prueba.py
@@ -0,0 +1,17 @@
+
+numer_to_guest = 2
+
+
+user_number = int (input("Adivina el numero: "))
+
+if numer_to_guest == user_number:
+    print ("Has ganado")
+else:
+    print ("Perdiste")
+
+
+
+
+
+
+
