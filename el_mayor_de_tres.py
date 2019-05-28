4a0246ed9f546eb2b61469191e6feb47a630e9e4
Nicolasgeekit <nicolas.salazar.it@gmail.com>
2019-05-28 12:59:43 -0300

    Initial commit

---
+++ el_mayor_de_tres.py
@@ -0,0 +1,11 @@
+
+
+primer_numero = int (input("primer numero: "))
+segundo_numero = int (input ("Segundo numero: "))
+tercer_numero = int (input ("tercer numero: "))
+
+
+resultado = max ( primer_numero, segundo_numero, tercer_numero )
+
+
+print ("el numero mas grande es: {}".format(resultado))
